1df42ace02c554568526b03dd574285dc83d8b96
Anton Vahlenberg <anton.vahlenberg@learnet.se>
2023-09-26 10:55:44 +0200

    gravity

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -7,6 +7,8 @@ public class CharacterController : MonoBehaviour
 {
 
     public float MovementSpeedPerSecond = 25.0f;
+    public float GravityPerSecond = 200.0f;
+    public float GroundLevel = 0.0f;
     void Start()
     {
         
@@ -15,19 +17,27 @@ public class CharacterController : MonoBehaviour
     void Update()
     {
 
+        Vector3 gravityPosition = gameObject.transform.position;
+        gravityPosition.y -= GravityPerSecond * Time.deltaTime;
+        if (gravityPosition.y < GroundLevel)
+        { 
+            gravityPosition.y = GroundLevel; 
+        }
+        gameObject.transform.position = gravityPosition;
+
         if (Input.GetKey(KeyCode.W))
         {
             Vector3 characterPosition = gameObject.transform.position; 
-            characterPosition.y += MovementSpeedPerSecond * Time.deltaTime;
+            characterPosition.y += MovementSpeedPerSecond * Time.deltaTime * 15;
             gameObject.transform.position = characterPosition;
         }
 
-        if (Input.GetKey(KeyCode.S))
-        {
-            Vector3 characterPosition = gameObject.transform.position;
-            characterPosition.y -= MovementSpeedPerSecond * Time.deltaTime;
-            gameObject.transform.position = characterPosition;
-        }
+        //if (Input.GetKey(KeyCode.S))
+        //{
+        //    Vector3 characterPosition = gameObject.transform.position;
+        //    characterPosition.y -= MovementSpeedPerSecond * Time.deltaTime;
+        //    gameObject.transform.position = characterPosition;
+        //}
 
         if (Input.GetKey(KeyCode.A))
         {
