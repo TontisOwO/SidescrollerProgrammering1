9c0829f0a9d1a6fa63ebec0d6b3ab983b6b26f93
Anton Vahlenberg <anton.vahlenberg@learnet.se>
2023-11-21 10:56:29 +0100

    PlayerData : ScriptableObject

--- Assets/Scripts/PlayerData.cs
+++ Assets/Scripts/PlayerData.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 [CreateAssetMenu(menuName = "MY STUFF/Savefile")]
-public class PlayerData : MonoBehaviour
+public class PlayerData : ScriptableObject
 {
     public int PlayerPoints = 0;
 }
