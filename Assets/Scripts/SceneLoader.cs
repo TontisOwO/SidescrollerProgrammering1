4d2a37bb01a84c298b14a7e9ff4ba84a584f0254
Anton Vahlenberg <anton.vahlenberg@learnet.se>
2023-12-15 08:23:19 +0100

    this time for sure

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -4,6 +4,21 @@ using UnityEngine;
 
 public class SceneLoader : MonoBehaviour
 {
+    private void Start()
+    {
+        SetRatio(4, 3);
+    }
+    void SetRatio(float w, float h)
+    {
+        if ((((float)Screen.width) / ((float)Screen.height)) > w / h)
+        {
+            Screen.SetResolution((int)(((float)Screen.height) * (w / h)), Screen.height, true);
+        }
+        else
+        {
+            Screen.SetResolution(Screen.width, (int)(((float)Screen.width) * (h / w)), true);
+        }
+    }
     public void LoadScene(string aSceneName)
     {
         UnityEngine.SceneManagement.SceneManager.LoadScene(aSceneName);
